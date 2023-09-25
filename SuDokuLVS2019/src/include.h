265537a38d53c77e41d17abd3328a499e9f860e4
Tekkenfede <94874090+Tekkenfede@users.noreply.github.com>
2023-09-25 13:07:37 +0200

    Update include.h

--- SuDokuLVS2019/src/include.h
+++ SuDokuLVS2019/src/include.h
@@ -8,10 +8,10 @@
 #include <SDL2/SDL_mixer.h>
 //#include <mikmod.h>
 #else
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_mixer.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h>
+#include <SDL/SDL_mixer.h>
 #endif
 
 #include <iostream>
